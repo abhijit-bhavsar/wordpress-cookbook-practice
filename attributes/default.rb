893402ada22451e9e78347fe149767600d603cb0
root <root@ip-192-168-1-63.ec2.internal>
2018-03-22 01:51:09 +0000

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['wordpress']['base_dir'] = '/var/www/html'
+default['wordpress']['git_repo'] = 'github.com/patilmahadev/wordpress.git'
+default['wordpress']['dbname'] = 'wordpress'
+default['wordpress']['dbuser'] = 'root'
+default['wordpress']['dbpassword'] = 'root1234'
+default['wordpress']['dbhost'] = 'localhost'
+default['wordpress']['dbport'] = '3306'
---
+++ recipes/install.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook:: wordpress
+# Recipe:: install
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package ['httpd', 'mysql', 'mysql-server', 'php', 'php-mysql'] do
+  action :install
+end
+
+git node['wordpress']['base_dir'] do
+  repository "git://#{node['wordpress']['git_repo']}"
+  revision 'master'
+  action :sync
+end
+
+bash 'set mysql root password' do
+  code <<-EOH
+    mysql -u root -e "create database #{node['wordpress']['dbname']}"
+    mysql -u root -e "SET PASSWORD FOR #{node['wordpress']['dbuser']}@'localhost' = PASSWORD(#{node['wordpress']['dbpassword']});"
+  EOH
+  action :run
+end
+
+template "#{node['wordpress']['base_dir']}/wp-config.php" do
+  source 'wp-config.php.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+  variables(
+    :dbname => node['wordpress']['dbname'],
+    :dbuser => node['wordpress']['dbuser'],
+    :dbpassword => node['wordpress']['dbpassword'],
+    :dbhost => node['wordpress']['dbhost'],
+    :dbport => node['wordpress']['dbport']
+  )
+  notifies :restart, 'service[httpd]', :delayed
+end
+
+service 'mysqld' do
+  action [:enable, :start]
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
